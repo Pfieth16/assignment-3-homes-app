2fe9f70c1f0388ea5cce1d5acb5c660b8b8c7455
Ethan P <pfieth16@gmail.com>
2026-03-18 15:35:19 -0500

    Completed Milestone 10

--- src/app/details/details.ts
+++ src/app/details/details.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing-service';
+import { Housinglocationinfo } from '../housinglocationinfo';
+import { HousingLocation } from '../housing-location/housing-location';
 
 @Component({
   selector: 'app-details',
@@ -7,5 +11,13 @@ import { Component } from '@angular/core';
   styleUrl: './details.css',
 })
 export class Details {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId = -1;
+  housingService = inject(HousingService);
+  housingLocation: Housinglocationinfo | undefined;
 
+  constructor() {
+    this.housingLocationId = Number(this.route.snapshot.params['id']);
+    this.housingLocation = this.housingService.getHousingLocationById(this.housingLocationId);
+  }
 }
